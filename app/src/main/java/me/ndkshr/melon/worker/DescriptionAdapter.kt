f1629f28872cf13ba424c040157138fe4ff495f8
ndkshr <nandugops@gmail.com>
2023-09-26 16:12:40 -0700

    MEL-1: Adds Background Service starter code

---
+++ app/src/main/java/me/ndkshr/melon/worker/DescriptionAdapter.kt
@@ -0,0 +1,29 @@
+package me.ndkshr.melon.worker
+
+import android.app.PendingIntent
+import android.graphics.Bitmap
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
+import com.google.android.exoplayer2.ui.PlayerNotificationManager.MediaDescriptionAdapter
+
+class DescriptionAdapter: MediaDescriptionAdapter {
+    override fun getCurrentContentTitle(player: Player): CharSequence {
+        return player.currentMediaItem?.mediaMetadata?.title ?: "Unknown Title"
+    }
+
+    override fun createCurrentContentIntent(player: Player): PendingIntent? {
+
+        val openAppIntent = Intent()
+    }
+
+    override fun getCurrentContentText(player: Player): CharSequence? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCurrentLargeIcon(
+        player: Player,
+        callback: PlayerNotificationManager.BitmapCallback
+    ): Bitmap? {
+        TODO("Not yet implemented")
+    }
+}
